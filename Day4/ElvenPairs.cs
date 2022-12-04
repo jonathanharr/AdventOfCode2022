cb8f76065f66a4343ca33353fb6d92fdc27ea2ba
DieselWeazel <jonathan.harr92@gmail.com>
2022-12-04 11:11:22 +0100

    Day 4 of elven work

---
+++ Day4/ElvenPairs.cs
@@ -0,0 +1,49 @@
+﻿namespace Day4;
+
+public class ElvenPairs
+{
+    public Section ElvenSectionFirst { get; set; }
+    public Section ElvenSectionSecond { get; set; }
+
+    public ElvenPairs(Section elvenSectionFirst, Section elvenSectionSecond)
+    {
+        ElvenSectionFirst = elvenSectionFirst;
+        ElvenSectionSecond = elvenSectionSecond;
+    }
+
+    public bool OverLapsOneOrTheOther()
+    {
+        return SectionAContainsSectionIdWithinRangeFromSectionB(ElvenSectionFirst, ElvenSectionSecond) 
+               || SectionAContainsSectionIdWithinRangeFromSectionB(ElvenSectionSecond, ElvenSectionFirst);
+    }
+
+    private bool SectionAContainsSectionIdWithinRangeFromSectionB(Section sectionA, Section sectionB)
+    {
+        var secondLowerBoundary = sectionB.LowerBoundary;
+        var secondUpperBoundary = sectionB.UpperBoundary;
+        return sectionA.ContainsSectionIdWithinRange(secondLowerBoundary)
+               || sectionA.ContainsSectionIdWithinRange(secondUpperBoundary);
+    }
+
+    public bool IsContainedByOneOrTheOther()
+    {
+        return SectionAContainsSectionB(ElvenSectionFirst, ElvenSectionSecond)
+               || SectionAContainsSectionB(ElvenSectionSecond, ElvenSectionFirst);
+    }
+
+    private bool SectionAContainsSectionB(Section sectionA, Section sectionB)
+    {
+        return LowerBoundaryIsLessThanOrEqualTo(sectionA, sectionB)
+               && UpperBoundaryIsMoreThanOrEqualTo(sectionA, sectionB);
+    }
+
+    private bool LowerBoundaryIsLessThanOrEqualTo(Section firstSection, Section otherSection)
+    {
+        return firstSection.LowerBoundary <= otherSection.LowerBoundary;
+    }
+
+    private bool UpperBoundaryIsMoreThanOrEqualTo(Section firstSection, Section otherSection)
+    {
+        return firstSection.UpperBoundary >= otherSection.UpperBoundary;
+    }
+}
---
+++ Day4/Section.cs
@@ -0,0 +1,18 @@
+﻿namespace Day4;
+
+public class Section
+{
+    public int LowerBoundary { get; set; }
+    public int UpperBoundary { get; set; }
+
+    public Section(int lowerBoundary, int upperBoundary)
+    {
+        LowerBoundary = lowerBoundary;
+        UpperBoundary = upperBoundary;
+    }
+
+    public bool ContainsSectionIdWithinRange(int sectionId)
+    {
+        return sectionId >= LowerBoundary && sectionId <= UpperBoundary;
+    }
+}
